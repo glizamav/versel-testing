d4b664344b808fd5fd4ef492c05eecf49f1e08bd
glizamav <44268738+glizamav@users.noreply.github.com>
2021-12-09 17:27:40 -0300

    Add files via upload

--- src/app/vistas/registro/registro.component.ts
+++ src/app/vistas/registro/registro.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-registro',
@@ -7,9 +9,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistroComponent implements OnInit {
 
-  constructor() { }
+  formContacto = new FormGroup({
+    nombre: new FormControl('',[Validators.required, Validators.minLength(5)]),
+    email: new FormControl('',[Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required, Validators.minLength(6)]),
+    password2: new FormControl('', [Validators.required, Validators.minLength(6)])
+    
+  });
+
+
+  constructor(private router: Router) { }
+
+ 
 
   ngOnInit(): void {
   }
 
+  submit(){
+    const {nombre,email,password,password2} = this.formContacto.value;
+    if(this.formContacto.valid){
+      let dataList = this.formContacto.value.password;
+      let dataList2 = this.formContacto.value.password2;
+      console.log("Datos: ", dataList);
+      console.log("Datos: ", dataList2);
+      if (dataList == dataList2){ 
+        this.router.navigate(['/']);}
+    } else alert("campos incompletos");
+  }
 }
