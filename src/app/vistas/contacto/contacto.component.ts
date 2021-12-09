3cbb1a0142515ba4e02090bb9696a9000d41859a
Yerko Herrera Alarcon <yherrala@emeal.nttdata.com>
2021-12-09 18:00:41 -0300

    Inclusion de Contacto

---
+++ src/app/vistas/contacto/contacto.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-contacto',
+  templateUrl: './contacto.component.html',
+  styleUrls: ['./contacto.component.css']
+})
+export class ContactoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  submit(){
+    console.log("Recibido")
+    alert("¡Gracias por tus comentarios!");
+  }
+
+}
