85a3a462bf65fd699bb90e12415ef0be5f3848cd
glizamav <44268738+glizamav@users.noreply.github.com>
2021-12-10 17:03:36 -0300

    Add files via upload

---
+++ src/app/vistas/envio/envio.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-envio',
+  templateUrl: './envio.component.html',
+  styleUrls: ['./envio.component.css']
+})
+export class EnvioComponent implements OnInit {
+
+
+  formEnvio = new FormGroup({
+    pais: new FormControl('',[Validators.required, Validators.minLength(4)]),
+    calle: new FormControl('',[Validators.required, Validators.minLength(4)]),
+    cuidad: new FormControl('',[Validators.required, Validators.minLength(4)]),
+    region: new FormControl('',[Validators.required, Validators.minLength(4)]),
+    codigoPostal: new FormControl('',[Validators.required, Validators.minLength(4)]),
+   
+  });
+
+
+
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+  }
+
+  submit(){
+    const {pais,calle,cuidad,region,codigoPostal} = this.formEnvio.value;
+    if(this.formEnvio.valid) this.router.navigate(['/']);
+    else alert("campos incompletos");
+  }
+}
