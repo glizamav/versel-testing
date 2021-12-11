c7dd64fada22caf10876142b933cf37d8ecabd7b
Christopher <55823055+Chris-dot@users.noreply.github.com>
2021-12-10 22:28:22 -0300

    Add files via upload

---
+++ src/app/vistas/pago/pago.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pago',
+  templateUrl: './pago.component.html',
+  styleUrls: ['./pago.component.css']
+})
+export class PagoComponent implements OnInit {
+
+//   var ticket = "111122";
+//   var reg = new RegExp('^[0-9]*$');
+
+// if (reg.test(ticket)==false) {
+//   alert('Only Numeric Ticket ID is allowed.');
+//   return false;
+// }
+ 
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
