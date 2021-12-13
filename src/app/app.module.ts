8e7a0ec87abde5d9922f0759f319c7fb0bc5de96
Javiera Ayala Navarro <jayalnav@emeal.nttdata.com>
2021-12-13 11:33:10 -0300

    api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './vistas/home/home.component';
@@ -26,6 +26,7 @@ import { FinalCompraComponent } from './vistas/final-compra/final-compra.compone
     FinalCompraComponent
   ],
   imports: [
+    HttpClientModule,
     BrowserModule,
     AppRoutingModule,
     FormsModule,
--- src/app/vistas/final-compra/final-compra.component.ts
+++ src/app/vistas/final-compra/final-compra.component.ts
@@ -1,5 +1,13 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
+interface gatitoImg{
+  breeds:any,
+  id: any,
+  url: any,
+  with: any,
+  height: any
+}
 @Component({
   selector: 'app-final-compra',
   templateUrl: './final-compra.component.html',
@@ -7,9 +15,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FinalCompraComponent implements OnInit {
 
-  constructor() { }
+  url ="";
+  object: any;
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
+    this.getGatito();
   }
 
+  getGatito(){
+    this.http.get<any>("https://api.thecatapi.com/v1/images/search").subscribe(
+      (res) => {
+        console.log(res);
+        this.url = res[0].url; 
+      }
+    )
+      
+  }
 }
